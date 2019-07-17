749a935d030b0b31a88ee95c813eb754fae6c8e1
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-17 18:42:25 +0200

    Added bounceCount with sound play

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 (function() {
   const canvas = document.querySelector('canvas');
   const ctx = canvas.getContext('2d');
+  const bounceTextField = document.querySelector('p');
+  let bounceCount = 0;
   let animationID;
-  const block1 = new Block(20, 400, 20, 1, ctx);
-  const block2 = new Block(200, 400, 40, -1, ctx);
+  const powerOf = 1;
+  const block1 = new Block(100, 400, 10, 1, 0, ctx);
+  const mass2 = Math.pow(100, powerOf);
+  const block2 = new Block(200, 400, 40, mass2, -1, ctx);
+  const sound = document.querySelector('audio');
+
+  const updateBounce = () => {
+    bounceCount++;
+    bounceTextField.innerHTML = `Bounce: ${bounceCount}`;
+    sound.currentTime = 0;
+    sound.play();
+  }
 
   canvas.width = 1000;
   canvas.height = 500;
@@ -18,8 +30,18 @@
     block2.draw();
     block1.update();
     block2.update();
-    block1.collide(block2);
-    block2.collide(block1);
+    if(block1.collide(block2)) {
+      const v1 = block1.bounce(block2);
+      const v2 = block2.bounce(block1);
+
+      block1.v = v1;
+      block2.v = v2
+      updateBounce();
+    }
+    if (block1.hitWall()) {
+      updateBounce();
+      block1.reverse();
+    }
   };
 
   document.addEventListener('keypress', e => {
